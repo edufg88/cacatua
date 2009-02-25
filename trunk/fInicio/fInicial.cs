92c365458ab263a6ad06e81feeb6bc1670d66c1f
jcz1710 <jcz1710@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-02-25 12:16:56 +0000

    Trozo base

---
+++ trunk/fInicio/fInicial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace fInicio
+{
+    public partial class fInicio : Form
+    {
+        public fInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void bSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bEntrar_Click(object sender, EventArgs e)
+        {
+            formularioBase f = new formularioBase();
+            f.Owner = this;
+            f.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ trunk/fInicio/formularioBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace fInicio
+{
+    public partial class formularioBase : Form
+    {
+        public formularioBase()
+        {
+            InitializeComponent();
+        }
+
+        private void formularioBase_Load(object sender, EventArgs e)
+        {
+            fInicio.ActiveForm.Visible = false;
+        }
+    }
+}
