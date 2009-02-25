ca686954ee2437b2685e7e5b931675dca0577b5f
jose.va18 <jose.va18@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-02-25 17:20:58 +0000

    Ahora subo los archivos

---
+++ trunk/cacatUA/cacatUA/FormLogin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cacatUA
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+            // Limpiamos la etiqueta de mensajes
+            label_mensaje.Text = "";
+        }
+
+        private void button_conectar_Click(object sender, EventArgs e)
+        {
+            // Validamos los datos
+            string usuario = textBox_usuario.Text;
+            string contraseña = textBox_contraseña.Text;
+            bool correcto = Login.validarDatos(usuario, contraseña);
+            if (correcto == true)
+            {
+                // Mostramos el panel de administración
+                FormGeneral panel = new FormGeneral();
+                //FormPanelAdministracion panel = new FormPanelAdministracion();
+                panel.Show();
+                this.Hide();
+            }
+            else
+            {
+                // Mostramos el mensaje de error
+                label_mensaje.Text = "Error: Usuario o contraseña no válidos";
+            }
+        }
+
+        private void button_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ trunk/cacatUA/cacatUA/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cacatUA
+{
+    class Login
+    {
+        static public bool validarDatos(string usuario, string contraseña)
+        {
+            // Validamos los datos
+            bool correcto = false;
+            if (usuario == "cacatua" && contraseña == "123456")
+                correcto = true;
+            return correcto;
+        }
+    }
+}
