ec4f7466da01cdea782d90a78f3c0a4db8967f66
jcz1710 <jcz1710@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-02-26 18:16:42 +0000

    prueba de interfaz para categorias

--- cacatUA/cacatUA/FormCategorias.cs
+++ cacatUA/cacatUA/FormCategorias.cs
@@ -14,5 +14,27 @@ namespace cacatUA
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listSecciones.SelectedIndex != -1)
+            {
+                listCategorias.Items.Clear();
+                listCategorias.Items.Add("Primer curso");
+                listCategorias.Items.Add("Segundo curso");
+                listCategorias.Items.Add("Tercer curso");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listCategorias.SelectedIndex != -1)
+            {
+                listSubcategorias.Items.Clear();
+                listSubcategorias.Items.Add("DPAA");
+                listSubcategorias.Items.Add("FAC");
+                listSubcategorias.Items.Add("SIE");
+            }
+        }
     }
 }
