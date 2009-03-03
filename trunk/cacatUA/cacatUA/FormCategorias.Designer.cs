7a8ef479faf565239695ec4e9dff535a67160744
jcz1710 <jcz1710@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-03 21:41:54 +0000

    Mas de interfaz de categorias (lo de jose va al pelo)

--- trunk/cacatUA/cacatUA/FormCategorias.Designer.cs
+++ trunk/cacatUA/cacatUA/FormCategorias.Designer.cs
@@ -28,42 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("IB");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Primero", new System.Windows.Forms.TreeNode[] {
-            treeNode1});
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Segundo");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Grupo del martes");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("DPAA", new System.Windows.Forms.TreeNode[] {
-            treeNode4});
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Tercero", new System.Windows.Forms.TreeNode[] {
-            treeNode5});
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Informatica", new System.Windows.Forms.TreeNode[] {
-            treeNode2,
-            treeNode3,
-            treeNode6});
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Biologia");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Matematicas");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("IB");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Primero", new System.Windows.Forms.TreeNode[] {
+            treeNode10});
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Segundo");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Grupo del martes");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("DPAA", new System.Windows.Forms.TreeNode[] {
+            treeNode13});
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Tercero", new System.Windows.Forms.TreeNode[] {
+            treeNode14});
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Informatica", new System.Windows.Forms.TreeNode[] {
+            treeNode11,
+            treeNode12,
+            treeNode15});
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Biologia");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Matematicas");
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.panel6.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -103,8 +105,9 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.ForeColor = System.Drawing.SystemColors.Desktop;
             this.panel1.Location = new System.Drawing.Point(3, 34);
@@ -114,78 +117,20 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.button8);
-            this.groupBox1.Controls.Add(this.button7);
-            this.groupBox1.Controls.Add(this.button6);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.listBox1);
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Location = new System.Drawing.Point(17, 12);
+            this.groupBox1.Location = new System.Drawing.Point(296, 40);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(384, 359);
+            this.groupBox1.Size = new System.Drawing.Size(387, 120);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Categoria";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(22, 129);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 20);
-            this.textBox2.TabIndex = 12;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(214, 299);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(100, 25);
-            this.button8.TabIndex = 11;
-            this.button8.Text = "Suscribir usuario";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(214, 254);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(100, 25);
-            this.button7.TabIndex = 10;
-            this.button7.Text = "Quitar usuario";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(214, 209);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(100, 25);
-            this.button6.TabIndex = 9;
-            this.button6.Text = "Ver usuario";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 177);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Usuarios suscritos:";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(30, 205);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(165, 121);
-            this.listBox1.TabIndex = 7;
+            this.groupBox1.Text = "Administrar";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(203, 37);
+            this.button5.Location = new System.Drawing.Point(192, 37);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(151, 23);
             this.button5.TabIndex = 5;
@@ -199,18 +144,9 @@
             this.textBox1.Size = new System.Drawing.Size(150, 20);
             this.textBox1.TabIndex = 4;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(203, 74);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(151, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Ver materiales";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(21, 74);
+            this.button4.Location = new System.Drawing.Point(22, 80);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(151, 23);
             this.button4.TabIndex = 2;
@@ -219,41 +155,12 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(203, 129);
+            this.button2.Location = new System.Drawing.Point(192, 80);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(151, 23);
             this.button2.TabIndex = 0;
             this.button2.Text = "Crear subcategoria";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(477, 9);
-            this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Nodo6";
-            treeNode1.Text = "IB";
-            treeNode2.Name = "Nodo5";
-            treeNode2.Text = "Primero";
-            treeNode3.Name = "Nodo7";
-            treeNode3.Text = "Segundo";
-            treeNode4.Name = "Nodo10";
-            treeNode4.Text = "Grupo del martes";
-            treeNode5.Name = "Nodo9";
-            treeNode5.Text = "DPAA";
-            treeNode6.Name = "Nodo8";
-            treeNode6.Text = "Tercero";
-            treeNode7.Name = "Nodo0";
-            treeNode7.Text = "Informatica";
-            treeNode8.Name = "Nodo1";
-            treeNode8.Text = "Biologia";
-            treeNode9.Name = "Nodo2";
-            treeNode9.Text = "Matematicas";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode7,
-            treeNode8,
-            treeNode9});
-            this.treeView1.Size = new System.Drawing.Size(206, 359);
-            this.treeView1.TabIndex = 2;
             // 
             // panel6
             // 
@@ -280,6 +187,113 @@
             this.label8.TabIndex = 7;
             this.label8.Text = "Categorías";
             // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(38, 40);
+            this.treeView1.Name = "treeView1";
+            treeNode10.Name = "Nodo6";
+            treeNode10.Text = "IB";
+            treeNode11.Name = "Nodo5";
+            treeNode11.Text = "Primero";
+            treeNode12.Name = "Nodo7";
+            treeNode12.Text = "Segundo";
+            treeNode13.Name = "Nodo10";
+            treeNode13.Text = "Grupo del martes";
+            treeNode14.Name = "Nodo9";
+            treeNode14.Text = "DPAA";
+            treeNode15.Name = "Nodo8";
+            treeNode15.Text = "Tercero";
+            treeNode16.Name = "Nodo0";
+            treeNode16.Text = "Informatica";
+            treeNode17.Name = "Nodo1";
+            treeNode17.Text = "Biologia";
+            treeNode18.Name = "Nodo2";
+            treeNode18.Text = "Matematicas";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode16,
+            treeNode17,
+            treeNode18});
+            this.treeView1.Size = new System.Drawing.Size(206, 369);
+            this.treeView1.TabIndex = 13;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(296, 166);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(386, 243);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Informacion";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(202, 202);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 25);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Suscribir usuario";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(202, 171);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 25);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Quitar usuario";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(202, 140);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 25);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Ver usuario";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Usuarios suscritos:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(165, 108);
+            this.listBox1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Materiales";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Foro";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormCategorias
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -293,6 +307,8 @@
             this.groupBox1.PerformLayout();
             this.panel6.ResumeLayout(false);
             this.panel6.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -302,20 +318,21 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Label label8;
 
     }
 }
