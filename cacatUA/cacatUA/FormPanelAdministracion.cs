a07427b450068df94ee2261feef47424bc01b611
jose.va18 <jose.va18@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-03 21:28:57 +0000

    me voy a cargar el repositorio, pero me la juego

--- cacatUA/cacatUA/FormPanelAdministracion.cs
+++ cacatUA/cacatUA/FormPanelAdministracion.cs
@@ -19,76 +19,62 @@ namespace cacatUA
         private void button_usuarios_Click(object sender, EventArgs e)
         {
             FormUsuarios form = new FormUsuarios();
-            form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            form.Location = new Point(this.Location.X, this.Location.Y);
-            form.Size = new Size(this.Size.Width, this.Size.Height);
-            form.Show();
-            this.Hide(); 
+            panel1.Controls.Clear();
+            panel1.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
         }
 
         private void button_foro_Click(object sender, EventArgs e)
         {
             FormForo form = new FormForo();
-            form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            form.Location = new Point(this.Location.X, this.Location.Y);
-            form.Size = new Size(this.Size.Width, this.Size.Height);
-            form.Show();
-            this.Hide(); 
+            panel1.Controls.Clear();
+            panel1.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
         }
 
         private void button_peticiones_Click(object sender, EventArgs e)
         {
             FormPeticiones form = new FormPeticiones();
-            form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            form.Location = new Point(this.Location.X, this.Location.Y);
-            form.Size = new Size(this.Size.Width, this.Size.Height);
-            form.Show();
-            this.Hide(); 
+            panel1.Controls.Clear();
+            panel1.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
         }
 
         private void button_grupos_Click(object sender, EventArgs e)
         {
             FormGrupos form = new FormGrupos();
-            form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            form.Location = new Point(this.Location.X, this.Location.Y);
-            form.Size = new Size(this.Size.Width, this.Size.Height);
-            form.Show();
-            this.Hide(); 
+            panel1.Controls.Clear();
+            panel1.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
         }
 
         private void button_materiales_Click(object sender, EventArgs e)
         {
             FormMateriales form = new FormMateriales();
-            form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            form.Location = new Point(this.Location.X, this.Location.Y);
-            form.Size = new Size(this.Size.Width, this.Size.Height);
-            form.Show();
-            this.Hide();
+            panel1.Controls.Clear();
+            panel1.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
         }
 
         private void button_general_Click(object sender, EventArgs e)
         {
             FormGeneral form = new FormGeneral();
-            form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            form.Location = new Point(this.Location.X, this.Location.Y);
-            form.Size = new Size(this.Size.Width, this.Size.Height);
-            form.Show();
-            this.Hide(); 
+            panel1.Controls.Clear();
+            panel1.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
         }
 
         private void button_categorias_Click(object sender, EventArgs e)
         {
             FormCategorias form = new FormCategorias();
-            form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            form.Location = new Point(this.Location.X, this.Location.Y);
-            form.Size = new Size(this.Size.Width, this.Size.Height);
-            form.Show();
-            this.Hide(); 
+            panel1.Controls.Clear();
+            panel1.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
         }
 
         private void FormPanelAdministracion_Load(object sender, EventArgs e)
         {
-            //this.WindowState = FormWindowState.Maximized;
+          //  this.WindowState = FormWindowState.Maximized;
         }
 
         private void button1_Click(object sender, EventArgs e)
