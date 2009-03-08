a33927c399b16bcf396657fb3dc61b681ab7d42b
Raulfc88 <Raulfc88@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-08 03:07:28 +0000

    Cambios en el formulario de peticiones

--- trunk/cacatUA/cacatUA/FormPeticiones.cs
+++ trunk/cacatUA/cacatUA/FormPeticiones.cs
@@ -14,6 +14,51 @@ namespace cacatUA
         public FormPeticiones()
         {
             InitializeComponent();
+            anadirAlgunasPeticiones();
+        }
+
+        private void anadirAlgunasPeticiones()
+        {
+            DataGridViewRow fila = new DataGridViewRow();
+            fila.CreateCells(dataGridViewRecientes);
+            fila.Cells[0].Value = "Nueva categoria";
+            fila.Cells[1].Value = "Jose";
+            dataGridViewRecientes.Rows.Add(fila);
+            DataGridViewRow fila1 = new DataGridViewRow();
+            fila1.CreateCells(dataGridViewRecientes);
+            fila1.Cells[0].Value = "Error Descarga Material";
+            fila1.Cells[1].Value = "Andres";
+            dataGridViewRecientes.Rows.Add(fila1);
+           
+
+        }
+
+        private void buttonUsuario_Click(object sender, EventArgs e)
+        {
+            /*Form1 form = new Form1();
+            form.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            form.Size = new Size(900, 500);
+            form.Show();*/
+        }
+        private void buttonMensajes_Click(object sender, EventArgs e)
+        {
+            /*Form1 form = new Form1();
+            form.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            form.Size = new Size(900, 500);
+            form.Show();*/
+        }
+
+        private void buttonRealizada_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewRecientes.SelectedRows != null)
+            {
+                /*DataGridViewRow fila = new DataGridViewRow();
+                fila.CreateCells(dataGridViewRecientes);
+                dataGridViewRecientes.SelectedCells.ToString();*/
+
+
+            }
+
         }
     }
 }
