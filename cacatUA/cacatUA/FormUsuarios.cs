ef240e5e2e2730d6eec2d925bc384d574e7a5821
edufg88 <edufg88@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-09 16:46:09 +0000

    Va un primer commit con casi todo lo de usuarios, tengo que acabar unos "DETALLITOS"

--- cacatUA/cacatUA/FormUsuarios.cs
+++ cacatUA/cacatUA/FormUsuarios.cs
@@ -15,5 +15,40 @@ namespace cacatUA
         {
             InitializeComponent();
         }
+
+        private void button_añadirUsuario_Click(object sender, EventArgs e)
+        {
+            FormUsuario form = new FormUsuario();
+            //form.setModo(FormUsuario.modos.NUEVO);
+            form.Show();
+        }
+
+        private void button_editarUsuario_Click(object sender, EventArgs e)
+        {
+            /* 
+            FormUsuario form = new FormUsuario();
+            form.setModo(FormUsuario.modos.EDITAR);
+            form.Show();
+             */ 
+        }
+
+        private void button_borrarUsuario_Click(object sender, EventArgs e)
+        {
+            /*
+            DialogResult resultado = MessageBox.Show("¿Estás seguro que deseas eliminar ese/esos USUARIO(S)?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (resultado == DialogResult.Yes)
+            {
+                // Obtenemos todas las filas seleccionada
+                DataGridViewSelectedRowCollection filas = dataGridViewUsuarios.SelectedRows;
+                for (int i = 0; i < filas.Count; i++)
+                {
+                    // Obtenemos la fila
+                    DataGridViewRow fila = filas[i];
+                    // Eliminamos la fila
+                    dataGridViewUsuarios.Rows.Remove(fila);
+                }
+            }
+             */ 
+        }
     }
 }
