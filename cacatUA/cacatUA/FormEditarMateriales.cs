d806301b71049c036422b4b7b2781180c2317035
jose.va18 <jose.va18@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-21 12:11:17 +0000

    He empezao a hacer lo de EN y CAD y toda la pesca de materiales. De momento, puedo crear materiales y mostrar todos los que hay. Para ello uso la bd que he instalado en mi pc. Si usais algo de materiales, como no teneis instalado la bd, seguramente os pete todo.

---
+++ cacatUA/cacatUA/FormEditarMateriales.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cacatUA
+{
+    public partial class FormEditarMateriales : UserControl
+    {
+
+        public enum modos { EDITAR = 0, CREAR = 1, BORRAR = 2};
+        public modos modo;
+        public FormEditarMateriales(modos modo)
+        {
+            InitializeComponent();
+            this.modo = modo;
+            switch (modo)
+            {
+                case modos.CREAR:
+                    {
+                        button_accion.Text = "Crear";
+                        break;
+                    }
+                case modos.EDITAR:
+                    {
+                        button_accion.Text = "Editar";
+                        break;
+                    }
+                case modos.BORRAR:
+                    {
+                        button_accion.Text = "Borrar";
+                        break;
+                    }
+            }
+        }
+
+        private void button_accion_Click(object sender, EventArgs e)
+        {
+            switch (modo)
+            {
+                case modos.CREAR:
+                    {
+                        // Creamos el nuevo material
+                        ENMaterialCRUD material = new ENMaterialCRUD();
+                        material.Nombre = textBox_nombre.Text.ToString();
+                        material.Descripcion = richTextBox_descripcion.Text.ToString();
+
+                        //material.Descripcion = "aaaaaa";
+                        // Comprobamos que 
+                        // Añadimos el material a la base de datos
+                        material.crearMaterial();
+
+                        break;
+                    }
+                case modos.EDITAR:
+                    {
+                        button_accion.Text = "Editar";
+                        break;
+                    }
+                case modos.BORRAR:
+                    {
+                        button_accion.Text = "Borrar";
+                        break;
+                    }
+            }
+        }
+    }
+}
