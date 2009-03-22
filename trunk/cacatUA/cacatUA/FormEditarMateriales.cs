d8a074c4a724cdfe84a9c4317d6a46ed0ffcd2fb
jose.va18 <jose.va18@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-22 12:42:07 +0000

    Más cosas en materiales. Ya se puede crear y borrar materiales (bd incluida) (falta validar datos). Me falta que se pueda editar y se reflejen los cambios en la bd y todo lo de buscar.

--- trunk/cacatUA/cacatUA/FormEditarMateriales.cs
+++ trunk/cacatUA/cacatUA/FormEditarMateriales.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -13,22 +15,162 @@ namespace cacatUA
 {
     public partial class FormEditarMateriales : UserControl
     {
-
+        string[] controlesCrear = { "nombre","descripcion","usuario","categoria","archivo","idioma","referencia" };
+        string[] controlesEditar = { "nombre", "descripcion", "fecha","usuario", "categoria", "archivo", "descargas","idioma","valoracion","votos","referencia" };
+        
         public enum modos { EDITAR = 0, CREAR = 1, BORRAR = 2};
-        public modos modo;
-        public FormEditarMateriales(modos modo)
+        private modos modo;
+        public modos Modo
+        {
+            get { return modo; }
+            set { modo = value; }
+        }
+
+        public FormEditarMateriales(modos modo, string id)
         {
             InitializeComponent();
             this.modo = modo;
+            inicializarControles();
+            actualizarFormulario(id);
+            
+        }
+
+        Dictionary<string, Control> controles = null;
+        private void inicializarControles()
+        {
+            controles = new Dictionary<string, Control>();
+            controles.Add("nombre", textBox_nombre);
+            controles.Add("descripcion", richTextBox_descripcion);
+            controles.Add("fecha", dateTimePicker_fecha);
+            controles.Add("usuario", textBox_usuario);
+            controles.Add("categoria", textBox_categoria);
+            controles.Add("archivo", textBox_archivo);
+            controles.Add("tamaño", textBox_tamaño);
+            controles.Add("descargas", textBox_descargas);
+            controles.Add("idioma", comboBox_idioma);
+            controles.Add("valoracion", textBox_valoracion);
+            controles.Add("votos", textBox_votos);
+            controles.Add("referencia", textBox_referencia);
+        }
+
+        private void prepararControles(string[] nombreControles)
+        {
+            limpiarFormulario();
+            // Activamos los controles que se indican y desactivamos el resto
+            foreach (KeyValuePair<string, Control> i in controles)
+            {
+                // Obtenemos el nombre
+                string nombre = i.Key;
+                // Obtenemos el control
+                Control control = i.Value;
+                // Comprobamos si el control está en el array de controles a activar
+                if (nombreControles.Contains(nombre) == true)
+                {
+                    // Lo activamos
+                    control.Enabled = true;
+                }
+                else
+                {
+                    // Lo desactivamos
+                    control.Enabled = false;
+                }
+            }
+        }
+
+        private void validarDatos(string[] nombreControles)
+        {
+            foreach (KeyValuePair<string, Control> i in controles)
+            {
+                // Obtenemos el nombre
+                string nombre = i.Key;
+                // Obtenemos el control
+                Control control = i.Value;
+                if (nombre == "nombre")
+                {
+
+
+                }
+            }
+            /*
+            limpiarFormulario();
+            // Activamos los controles que se indican y desactivamos el resto
+            foreach (KeyValuePair<string, Control> i in controles)
+            {
+                // Obtenemos el nombre
+                string nombre = i.Key;
+                // Obtenemos el control
+                Control control = i.Value;
+                // Comprobamos si el control está en el array de controles a activar
+                if (nombreControles.Contains(nombre) == true)
+                {
+                    // Lo activamos
+                    control.Enabled = true;
+                }
+                else
+                {
+                    // Lo desactivamos
+                    control.Enabled = false;
+                }
+            }*/
+        }
+
+        public void limpiarFormulario()
+        {
+            TextBox textBox;
+            textBox = (TextBox)controles["nombre"];
+            textBox.Clear();
+            RichTextBox richTextBox = (RichTextBox)controles["descripcion"];
+            richTextBox.Clear();
+            //dateTimePicker_fecha
+            textBox = (TextBox)controles["usuario"];
+            textBox.Clear();
+            textBox = (TextBox)controles["categoria"];
+            textBox.Clear();
+            textBox = (TextBox)controles["archivo"];
+            textBox.Clear();
+            textBox = (TextBox)controles["tamaño"];
+            textBox.Clear();
+            textBox = (TextBox)controles["descargas"];
+            textBox.Clear();
+            //comboBox_idioma.Clear();
+            textBox = (TextBox)controles["valoracion"];
+            textBox.Clear();
+            textBox = (TextBox)controles["votos"];
+            textBox.Clear();
+            textBox = (TextBox)controles["referencia"];
+            textBox.Clear();
+        }
+
+        public void actualizarFormulario(string id)
+        {
+            comboBox_idioma.SelectedIndex = 0;
+
             switch (modo)
             {
                 case modos.CREAR:
                     {
+                        prepararControles(controlesCrear);
                         button_accion.Text = "Crear";
                         break;
                     }
                 case modos.EDITAR:
                     {
+                        prepararControles(controlesEditar);
+                        // Obtenemos toda la información del material
+                        ENMaterialCRUD material = MaterialCAD.obtenerMaterial(int.Parse(id));
+                        controles["nombre"].Text = material.Nombre;
+                        controles["descripcion"].Text = material.Descripcion;
+                        // fecha
+                        controles["usuario"].Text = material.Usuario;
+                        controles["categoria"].Text = material.Categoria;
+                        controles["archivo"].Text = material.Archivo;
+                        controles["tamaño"].Text = material.Tamaño.ToString();
+                        controles["descargas"].Text = material.Descargas.ToString();
+                        // idioma
+                        controles["valoracion"].Text = material.Valoracion.ToString();
+                        controles["votos"].Text = material.Votos.ToString();
+                        controles["referencia"].Text = material.Referencia;
+
                         button_accion.Text = "Editar";
                         break;
                     }
@@ -46,29 +188,66 @@ namespace cacatUA
             {
                 case modos.CREAR:
                     {
+                        // Comprobamos que los campos sean válidos
+                        validarDatos(controlesCrear);
+
                         // Creamos el nuevo material
                         ENMaterialCRUD material = new ENMaterialCRUD();
                         material.Nombre = textBox_nombre.Text.ToString();
                         material.Descripcion = richTextBox_descripcion.Text.ToString();
-
-                        //material.Descripcion = "aaaaaa";
-                        // Comprobamos que 
+                        material.Usuario = textBox_usuario.Text.ToString();
+                        material.Categoria = textBox_categoria.Text.ToString();
+                        material.Archivo = textBox_archivo.Text.ToString();
+                        material.Tamaño = convertirTamaño(textBox_tamaño.Text.ToString());
+                        material.Idioma = comboBox_idioma.SelectedItem.ToString();
+                        material.Referencia = textBox_referencia.Text.ToString();
                         // Añadimos el material a la base de datos
                         material.crearMaterial();
-
                         break;
                     }
                 case modos.EDITAR:
                     {
-                        button_accion.Text = "Editar";
+                        
                         break;
                     }
                 case modos.BORRAR:
                     {
-                        button_accion.Text = "Borrar";
+                        
                         break;
                     }
             }
         }
+
+        private void seleccionarArchivo(object sender, EventArgs e)
+        {
+            // Creamos una ventana para que el usuario seleccione el material
+            OpenFileDialog dialogo = new OpenFileDialog();
+            dialogo.InitialDirectory = "C:\\";
+            if (dialogo.ShowDialog() == DialogResult.OK)
+            {
+                controles["archivo"].Text = dialogo.FileName;
+                FileInfo info = new FileInfo(dialogo.FileName);
+                long tamaño = info.Length;
+                if (tamaño > 1024)
+                {
+                    tamaño = tamaño / 1024;
+                    controles["tamaño"].Text = tamaño.ToString() + " KB";
+                }
+                else
+                {
+                    controles["tamaño"].Text = tamaño.ToString() + " bytes";
+                }
+            }
+        }
+
+        private int convertirTamaño(string tamaño)
+        {
+            // Quitamos la medida
+            Console.WriteLine(tamaño.IndexOf(' '));
+            tamaño = tamaño.Remove(tamaño.IndexOf(' '));
+            tamaño.Trim();
+            return int.Parse(tamaño);
+        }
+
     }
 }
--- trunk/cacatUA/cacatUA/FormMateriales.cs
+++ trunk/cacatUA/cacatUA/FormMateriales.cs
@@ -13,9 +13,21 @@ namespace cacatUA
 {
     public partial class FormMateriales : UserControl
     {
+        private bool busqueda;
+        FormEditarMateriales formEditarMateriales;
+
+       
+
         public FormMateriales()
         {
             InitializeComponent();
+            // Por defecto se muestra el formulario de búsqueda
+            busqueda = true;
+            formEditarMateriales = null;
+            FormBusquedaMateriales form = new FormBusquedaMateriales();
+            panel_contenido.Controls.Clear();
+            panel_contenido.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
             /*
             // Inicializamos las secciones
             comboBox_seccion.Items.Add("Ingeniería Informática");
@@ -43,6 +55,8 @@ namespace cacatUA
             crearMateriales();
         }
 
+
+
         private void crearMateriales()
         {
             // Obtenemos todos los materiales que hay en la base de datos
@@ -52,30 +66,29 @@ namespace cacatUA
             {
                 ENMaterialCRUD material = (ENMaterialCRUD)materiales[i];
                 DataGridViewRow fila = new DataGridViewRow();
-                fila.CreateCells(dataGridView1);
-                fila.Cells[0].Value = material.Nombre;
-                fila.Cells[1].Value = material.Descripcion;
-               //fila.Cells[2].Value = añadido;
-                //fila.Cells[3].Value = valoracion;
+                fila.CreateCells(dataGridViewMateriales);
+                int posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_id"].Index;
+                fila.Cells[posicion].Value = material.Id.ToString();
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_nombre"].Index;
+                fila.Cells[posicion].Value = material.Nombre;
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_descripcion"].Index;
+                fila.Cells[posicion].Value = material.Descripcion;
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_fecha"].Index;
+                fila.Cells[posicion].Value = material.Fecha.ToUniversalTime();
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_usuario"].Index;
+                fila.Cells[posicion].Value = material.Usuario;
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_categoria"].Index;
+                fila.Cells[posicion].Value = material.Categoria;
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_tamaño"].Index;
+                fila.Cells[posicion].Value = material.Tamaño;
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_descargas"].Index;
+                fila.Cells[posicion].Value = material.Descargas;
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_idioma"].Index;
+                fila.Cells[posicion].Value = material.Idioma;
+                posicion = dataGridViewMateriales.Columns["dataGridViewTextBoxColumn_valoracion"].Index;
+                fila.Cells[posicion].Value = material.Valoracion;                                        
                 dataGridViewMateriales.Rows.Add(fila);
             }
-
-            /*
-            for (int i = 0; i < 10; i++)
-            {
-                string nombre = "material" + i.ToString();
-                string descripcion = "descripcion material " + i.ToString();
-                string añadido = "01/01/2009";
-                string valoracion = "5";
-                DataGridViewRow fila = new DataGridViewRow();
-                fila.CreateCells(dataGridView1);
-                fila.Cells[0].Value = nombre;
-                fila.Cells[1].Value = descripcion;
-                fila.Cells[2].Value = añadido;
-                fila.Cells[3].Value = valoracion;
-                dataGridViewMateriales.Rows.Add(fila);
-            }
-             */
         }
 
         private void button_añadirMaterial_Click(object sender, EventArgs e)
@@ -85,16 +98,7 @@ namespace cacatUA
             form.Show();
         }
 
-        private void butto_editarMaterial_Click(object sender, EventArgs e)
-        {
-            /*
-            // Mostramos el user control de busqueda
-            FormEditarMateriales form = new FormEditarMateriales();
-            panel_contenido.Controls.Clear();
-            panel_contenido.Controls.Add(form);
-            form.Dock = DockStyle.Fill;
-             */
-        }
+
 
         private void button_borrarMaterial_Click(object sender, EventArgs e)
         {
@@ -122,10 +126,7 @@ namespace cacatUA
             form.ShowDialog();
         }
 
-        private void panel4_Paint(object sender, PaintEventArgs e)
-        {
 
-        }
 
         private void button_seccionBuscar_Click(object sender, EventArgs e)
         {
@@ -136,60 +137,127 @@ namespace cacatUA
             form.Dock = DockStyle.Fill;
         }
 
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
 
+        private void editarMaterial(object sender, EventArgs e)
+        {
+            // Comprobamos si hay algún material seleccionado 
+            DataGridViewSelectedRowCollection filas = dataGridViewMateriales.SelectedRows;
+            if(filas.Count == 0)
+            {
+                // No hay ningún material seleccionado, mostramos un aviso
+                MessageBox.Show("Debes seleccionar un material", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                DataGridViewRow fila = fila = filas[0];
+                // Comprobamos si hay un material seleccionado únicamente
+                if (filas.Count > 1)
+                {
+                    // Deseleccionamos el resto de elementos
+                    dataGridViewMateriales.ClearSelection();
+                    dataGridViewMateriales.Rows[fila.Index].Selected = true;
+                }
+
+                // Obtenemos el id del material que vamos a editar
+                string id = fila.Cells["dataGridViewTextBoxColumn_id"].Value.ToString();
+                // Comprobamos si el formulario visible es el de búsqueda
+                if (busqueda == true)
+                {
+                    // Mostramos el formulario de editar material
+                    formEditarMateriales = new FormEditarMateriales(FormEditarMateriales.modos.EDITAR, id);
+                    panel_contenido.Controls.Clear();
+                    panel_contenido.Controls.Add(formEditarMateriales);
+                    formEditarMateriales.Dock = DockStyle.Fill;
+
+                }
+                else
+                {
+                    // Recargamos el formulario editar material con los nuevos datos
+                    if (formEditarMateriales != null)
+                    {
+                        // Cambiamos el formulario al modo editar
+                        formEditarMateriales.Modo = FormEditarMateriales.modos.EDITAR;
+                        formEditarMateriales.actualizarFormulario(id);
+
+                    }
+                    //else
+                      //  formEditarMateriales = new FormEditarMateriales(FormEditarMateriales.modos.EDITAR, id);
+                }
+                busqueda = false;
+            }
         }
 
-        private void button_seccionBuscar_Click_1(object sender, EventArgs e)
+        private void borrarMaterial(object sender, EventArgs e)
         {
-            // Mostramos el user control de busqueda
+            // Comprobamos que haya algún material seleccionado
+            DataGridViewSelectedRowCollection filas = dataGridViewMateriales.SelectedRows;
+            if (filas.Count == 0)
+            {
+                // No hay ningún material seleccionado, mostramos un aviso
+                MessageBox.Show("Debes seleccionar un material", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                DialogResult resultado;
+                // Mostramos un mensaje de aviso para que el usuario confirme que quiere borrar el material
+                if (filas.Count == 1)
+                {
+                    string mensaje = "¿Está seguro de que desea eliminar " + filas[0].Cells["dataGridViewTextBoxColumn_nombre"].Value.ToString() + "?";
+                    resultado = MessageBox.Show(mensaje, "Confirmar eliminación de material", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                }
+                else
+                {
+                    string mensaje = "¿Está seguro que desea eliminar estos " + filas.Count + " materiales?";
+                    resultado = MessageBox.Show(mensaje, "Confirmar la eliminación de múltiples materiales", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                }
+                // Comprobamos cual ha sido el resultado
+                if (resultado == DialogResult.Yes)
+                {
+                    for (int i = 0; i < filas.Count; i++)
+                    {
+                        // Obtenemos la fila
+                        DataGridViewRow fila = filas[i];
+                        // Eliminamos el material de la base de datos
+                        int id = int.Parse(fila.Cells["dataGridViewTextBoxColumn_id"].Value.ToString());
+                        bool borrado = ENMaterialCRUD.borrarMaterial(id);
+                        if (borrado == false)
+                        {
+                            string mensaje = "ERROR: No se ha podido borrar el material " + id;
+                            MessageBox.Show(mensaje, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        // Eliminamos la fila
+                        dataGridViewMateriales.Rows.Remove(fila);
+                    }
+                }
+            }
+        }
+
+        private void añadirMaterial(object sender, EventArgs e)
+        {
+            // Comprobamos si el formulario visible es el de búsqueda
+            if (busqueda == true || formEditarMateriales == null)
+            {
+                // Añadimos un nuevo material
+                formEditarMateriales = new FormEditarMateriales(FormEditarMateriales.modos.CREAR, "");
+                panel_contenido.Controls.Clear();
+                panel_contenido.Controls.Add(formEditarMateriales);
+                formEditarMateriales.Dock = DockStyle.Fill;
+            }
+            else
+            {
+                formEditarMateriales.Modo = FormEditarMateriales.modos.CREAR;
+                formEditarMateriales.actualizarFormulario("");
+            }
+            busqueda = false;
+        }
+
+        private void buscarMateriales(object sender, EventArgs e)
+        {
+            busqueda = true;
             FormBusquedaMateriales form = new FormBusquedaMateriales();
             panel_contenido.Controls.Clear();
             panel_contenido.Controls.Add(form);
             form.Dock = DockStyle.Fill;
- 
-        }
-
-        private void butto_editarMaterial_Click_1(object sender, EventArgs e)
-        {
-            /*
-            // Mostramos el user control de busqueda
-            FormEditarMateriales form = new FormEditarMateriales();
-            panel_contenido.Controls.Clear();
-            panel_contenido.Controls.Add(form);
-            form.Dock = DockStyle.Fill;
-             */
-        }
-
-        private void editarMaterial(object sender, EventArgs e)
-        {
-            /*
-            // Mostramos el user control de busqueda
-            FormEditarMateriales form = new FormEditarMateriales();
-            panel_contenido.Controls.Clear();
-            panel_contenido.Controls.Add(form);
-            form.Dock = DockStyle.Fill;
-             */
-        }
-
-        private void button_añadirMaterial_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button_añadirMaterial_Click_2(object sender, EventArgs e)
-        {
-            // Añadimos un nuevo material
-            FormEditarMateriales form = new FormEditarMateriales(FormEditarMateriales.modos.CREAR);
-            panel_contenido.Controls.Clear();
-            panel_contenido.Controls.Add(form);
-            form.Dock = DockStyle.Fill;
-        }
-
-        private void dataGridViewMateriales_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
         }
     }
 }
