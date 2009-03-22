ad45edb4e4dd2f0dfab8217ef1ae6e0277afcb6c
jose.va18 <jose.va18@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-22 12:44:10 +0000

    No se ha hecho commit de la libreria

--- cacatUA/Libreria/MaterialCAD.cs
+++ cacatUA/Libreria/MaterialCAD.cs
@@ -13,65 +13,119 @@ namespace Libreria
     public class MaterialCAD
     {
         static string cadenaConexion = "Data Source=PORTATIL\\SQLEXPRESS;Initial Catalog=cacatuaDB;Integrated Security=True;Pooling=False";
+        
         public MaterialCAD()
         {
-            SqlConnection conexion = new SqlConnection(cadenaConexion);
-            conexion.Open();
-            Console.WriteLine(conexion.State.ToString());
-            /*
-            // Insertamos un usuario
-            string cadenaComando = "INSERT INTO Materiales(id,nombre,descripcion) VALUES ('2','jose','hola')";
-            SqlCommand comando = new SqlCommand(cadenaComando, conexion);
-            comando.ExecuteNonQuery();
-            */
-            conexion.Close();
+
+        }
+
+        private static ENMaterialCRUD obtenerDatos(SqlDataReader reader)
+        {
+            ENMaterialCRUD material = new ENMaterialCRUD();
+            material.Id = int.Parse(reader["id"].ToString());
+            material.Nombre = reader["nombre"].ToString();
+            material.Descripcion = reader["descripcion"].ToString();
+            //material.Fecha = reader["fecha"].ToString();
+            material.Usuario = reader["usuario"].ToString();
+            material.Categoria = reader["categoria"].ToString();
+            material.Archivo = reader["archivo"].ToString();
+            material.Tamaño = int.Parse(reader["tamaño"].ToString());
+            material.Descargas = int.Parse(reader["descargas"].ToString());
+            material.Idioma = reader["idioma"].ToString();
+            material.Valoracion = int.Parse(reader["valoracion"].ToString());
+            material.Votos = int.Parse(reader["votos"].ToString());
+            material.Referencia = reader["referencia"].ToString();
+            return material;
         }
 
         public static ArrayList obtenerMateriales()
         {
-            ArrayList materiales = new ArrayList(); 
-            
-            SqlConnection conexion = new SqlConnection(cadenaConexion);
-            conexion.Open();     
-            
-            string cadenaComando = "SELECT * FROM Materiales";
-            SqlCommand comando = new SqlCommand(cadenaComando, conexion);
-            SqlDataReader reader = comando.ExecuteReader();
-            // Recorremos el reader y vamos insertando en el array list objetos del tipo ENMaterialCRUD
-            while(reader.Read())
+            ArrayList materiales = new ArrayList();
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
             {
-                int id = int.Parse(reader["id"].ToString());
-                string nombre = reader["nombre"].ToString();
-                string descripcion = reader["descripcion"].ToString();
-                // Creamos el material
-                ENMaterialCRUD material = new ENMaterialCRUD();
-                material.Id = id;
-                material.Nombre = nombre;
-                material.Descripcion = descripcion;
-                materiales.Add(material);
+                conexion.Open();
+                string cadenaComando = "SELECT * FROM Materiales";
+                SqlCommand comando = new SqlCommand(cadenaComando, conexion);
+                SqlDataReader reader = comando.ExecuteReader();
+                // Recorremos el reader y vamos insertando en el array list objetos del tipo ENMaterialCRUD
+                while (reader.Read())
+                {
+                    ENMaterialCRUD material = obtenerDatos(reader);
+                    materiales.Add(material);
+                }
             }
-            conexion.Close();
             return materiales;
         }
 
-        public static void crearMaterial(string nombre, string descripcion)
+
+        public static ENMaterialCRUD obtenerMaterial(int id)
         {
-            SqlConnection conexion = new SqlConnection(cadenaConexion);
-            conexion.Open();
-            //string cadenaComando = string.Format("INSERT INTO Materiales(id,nombre,descripcion) VALUES ('6',{0},{1}",nombre,descripcion);
-            //string cadenaComando = "INSERT INTO Materiales(id,nombre,descripcion) VALUES ('2','as','lol')";
+            ENMaterialCRUD material = null;
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                // Abrimos la conexión
+                conexion.Open();
+                // Creamos el comando
+                SqlCommand comando = new SqlCommand();
+                // Le asignamos la conexión al comando
+                comando.Connection = conexion;
+                comando.CommandText = "SELECT * FROM materiales where id = @id";
+                comando.Parameters.AddWithValue("@id", id);
+                SqlDataReader reader = comando.ExecuteReader();
+                if (reader.Read())
+                {
+                    // Obtenemos la información
+                    material = obtenerDatos(reader);
+                }
+            }
+            return material;
+        }
 
-            SqlCommand comando = new SqlCommand();
-            comando.CommandText = "INSERT INTO Materiales(id,nombre,descripcion) " +
-              "VALUES ('2', @nombre, @descripcion)";
+        public static bool borrarMaterial(int id)
+        {
+            int resultado = 0;
+            bool borrado = false;
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                // Abrimos la conexión
+                conexion.Open();
+                // Creamos el comando
+                SqlCommand comando = new SqlCommand();
+                // Le asignamos la conexión al comando
+                comando.Connection = conexion;
+                comando.CommandText = "DELETE FROM materiales where id = @id";
+                comando.Parameters.AddWithValue("@id", id);
+                resultado = comando.ExecuteNonQuery();
+                if (resultado == 1)
+                    borrado = true;
+            }
+            return borrado;
+        }
 
-            comando.Parameters.AddWithValue("@nombre", nombre);
-            comando.Parameters.AddWithValue("@descripcion", descripcion);
-
-            comando.Connection = conexion;
-            
-            comando.ExecuteNonQuery();
-            
+        public static void crearMaterial(string nombre, string descripcion, string usuario, string categoria, 
+            string archivo, int tamaño, string idioma, string referencia)
+        {
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                // Abrimos la conexión
+                conexion.Open();
+                // Creamos el comando
+                SqlCommand comando = new SqlCommand();
+                // Le asignamos la conexión al comando
+                comando.Connection = conexion;
+                comando.CommandText = "INSERT INTO " +
+                    "Materiales(nombre,descripcion,usuario,categoria,archivo,tamaño,idioma,referencia) " +
+                    "VALUES (@nombre,@descripcion,@usuario,@categoria,@archivo,@tamaño,@idioma,@referencia)";
+                comando.Parameters.AddWithValue("@nombre", nombre);
+                comando.Parameters.AddWithValue("@descripcion", descripcion);
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@categoria", categoria);
+                comando.Parameters.AddWithValue("@archivo", archivo);
+                comando.Parameters.AddWithValue("@tamaño", tamaño);
+                comando.Parameters.AddWithValue("@idioma", idioma);
+                comando.Parameters.AddWithValue("@referencia", referencia);
+                comando.ExecuteNonQuery();
+            }
             /*
             comando.Parameters["@Date"].Value = DateTime.Now.ToString();
             comando.Parameters.Add("@From", SqlDbType.NVarChar);
@@ -79,8 +133,6 @@ namespace Libreria
             myCmd.Parameters.Add("@Mail", SqlDbType.NVarChar);
             myCmd.Parameters["@Mail"].Value = msgEmail.Value;
            */
-          
-            conexion.Close();
         }
     }
 }
