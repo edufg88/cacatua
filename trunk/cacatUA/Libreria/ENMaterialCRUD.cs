77c33fc204e39d3fc41d0dc9cd119991229a0bc1
jose.va18 <jose.va18@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-22 12:44:10 +0000

    No se ha hecho commit de la libreria

--- trunk/cacatUA/Libreria/ENMaterialCRUD.cs
+++ trunk/cacatUA/Libreria/ENMaterialCRUD.cs
@@ -8,31 +8,35 @@ namespace Libreria
 {
     public class ENMaterialCRUD
     {
-        // Dispone de un materialCAD para acceder a los datos
         private int id;
         private string nombre;
         private string descripcion;
-
-        public int Id
-        {
-            get {return id;}
-            set { id = value; }
-        }
-
-        public string Nombre
-        {
-            get {return nombre;}
-            set { nombre = value; }
-        }
-
-        public string Descripcion
-        {
-            get {return descripcion;}
-            set { descripcion = value; }
-        }
+        private DateTime fecha;
+        private string usuario;
+        private string categoria;
+        private string archivo;
+        private int tamaño;
+        private int descargas;
+        private string idioma;
+        private int valoracion;
+        private int votos;
+        private string referencia;
 
         public ENMaterialCRUD()
         {
+            // Inicializamos
+            id = 0;
+            nombre = "";
+            descripcion = "";
+            usuario = "";
+            categoria = "";
+            archivo = "";
+            tamaño = 0;
+            descargas = 0;
+            idioma = "";
+            valoracion = 0;
+            votos = 0;
+            referencia = "";
             //materialCAD = new MaterialCAD();
         }
 
@@ -44,8 +48,95 @@ namespace Libreria
 
         public void crearMaterial()
         {
-            MaterialCAD.crearMaterial(nombre,descripcion);
+            MaterialCAD.crearMaterial(nombre,descripcion,usuario,categoria,archivo,tamaño,idioma,referencia);
         }
 
+        public bool borrarMaterial()
+        {
+            return MaterialCAD.borrarMaterial(id);
+        }
+
+        public static bool borrarMaterial(int id)
+        {
+            return MaterialCAD.borrarMaterial(id);
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        public string Categoria
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+
+        public string Archivo
+        {
+            get { return archivo; }
+            set { archivo = value; }
+        }
+
+        public int Tamaño
+        {
+            get { return tamaño; }
+            set { tamaño = value; }
+        }
+
+        public int Descargas
+        {
+            get { return descargas; }
+            set { descargas = value; }
+        }
+
+        public string Idioma
+        {
+            get { return idioma; }
+            set { idioma = value; }
+        }
+
+        public int Valoracion
+        {
+            get { return valoracion; }
+            set { valoracion = value; }
+        }
+
+        public int Votos
+        {
+            get { return votos; }
+            set { votos = value; }
+        }
+
+        public string Referencia
+        {
+            get { return referencia; }
+            set { referencia = value; }
+        }
     }
 }
