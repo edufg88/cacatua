c6d4f11f0a4852af30fe99e1d6db3ec0932fd782
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-29 21:47:58 +0000

    Clases para el foro. Sólo están empezadas, para plantearme el percal. Falta construir las sentencias SQL e integrarlo en el formulario.

---
+++ cacatUA/Libreria/ENRespuestaCRUD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Libreria
+{
+    class ENRespuestaCRUD
+    {
+        private int id;
+        private String texto;
+        //private ENUsuarioCRUD autor;
+        private DateTime fecha;
+
+        public ENRespuestaCRUD()
+        {
+            id = 0;
+            texto = "";
+            //autor = null;
+            fecha = new DateTime();
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public String Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+
+        /*public ENUsuarioCRUD Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }*/
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+    }
+}
---
+++ cacatUA/Libreria/HiloCAD.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Libreria
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class HiloCAD
+    {
+        private static String conexion = @"Data Source=COMPUTORO\SQLEXPRESS;Initial Catalog=cacatua;Integrated Security=True;Pooling=False";
+
+        public static ENHiloCRUD Obtener(int id)
+        {
+            // select * from hilos where id = id;
+            return null;
+        }
+
+        public static ArrayList Obtener()
+        {
+            // select * from hilos;
+            return null;
+        }
+
+        public static ArrayList Obtener(int pagina, int cantidad)
+        {
+            // select * from hilos limit pagina*cantidad, cantidad;
+            return null;
+        }
+
+        public static ArrayList Obtener(int pagina, int cantidad, String titulo, String texto
+            /*, ENUsuarioCRUD autor*/, DateTime fechaInicio, DateTime fechaFin, ENCategoriaCRUD categoria)
+        {
+            // select *
+            // from hilos
+            // where titulo like '%titulo%'
+            // and texto like '%texti%'
+            // and fecha between fechaInicio at fechaFin
+            // and categoria = categoria.id
+            // and limit pagina*cantidad, cantidad
+            return null;
+        }
+
+        public static void Guardar(ENHiloCRUD hilo)
+        {
+            // insert into hilos (...) values (...);
+        }
+
+        public static void Borrar(ENHiloCRUD hilo)
+        {
+            // delete from hilos where id = hilo.id;
+        }
+
+        public static void Borrar(int id)
+        {
+            // delete from hilos where id = id;
+        }
+
+        public static void Actualizar(ENHiloCRUD hilo)
+        {
+            // update hilos set tal tal tal where id = hilo.id;
+        }
+    }
+}
