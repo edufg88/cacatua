6923cd3e07bde5f2d8a9ceda1455be95725d4f4f
edufg88 <edufg88@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-30 22:21:28 +0000

    Subo cambios, he creado algunas entidades más con lo básico... Usuario tiene algo más...

---
+++ trunk/cacatUA/Libreria/ENFirmaCRUD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Libreria
+{
+    public class ENFirmaCRUD
+    {
+        private int id;
+        private ENUsuarioCRUD emisor;
+        private string texto;
+        private DateTime fecha;
+        private ENUsuarioCRUD receptor;
+
+        ENFirmaCRUD()
+        {
+            id = 0;
+            texto = "";
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public ENUsuarioCRUD Emisor
+        {
+            get { return emisor; }
+            set { emisor = value; }
+        }
+        public string Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+        public ENUsuarioCRUD Receptor
+        {
+            get { return receptor; }
+            set { receptor = value; }
+        }
+    }
+}
---
+++ trunk/cacatUA/Libreria/ENImagenCRUD.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Libreria
+{
+    public class ENImagenCRUD
+    {
+        private int id;
+        private string titulo;
+        private string descripcion;
+        private ENUsuarioCRUD usuario;
+        private string archivo;
+
+        ENImagenCRUD()
+        {
+            id = 0;
+            titulo = "";
+            descripcion = "";
+            archivo = "";
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Titulo
+        {
+            get { return Titulo; }
+            set { titulo = value; }
+        }
+        public string Descripcion
+        {
+            get { return Descripcion; }
+            set { descripcion = value; }
+        }
+        public ENUsuarioCRUD Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        public string Archivo
+        {
+            get { return archivo; }
+            set { archivo = value; }
+        }
+    }
+}
