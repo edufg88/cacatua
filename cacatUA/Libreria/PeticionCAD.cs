25e7f667c555ca38c00bfae8a1333384e852a672
damianmp <damianmp@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-31 08:25:40 +0000

    clase PeticionCAD

--- cacatUA/Libreria/PeticionCAD.cs
+++ cacatUA/Libreria/PeticionCAD.cs
@@ -7,16 +7,27 @@ namespace Libreria
 {
     class PeticionCAD
     {
-        /*public static ArrayList getSinContestar()
+        public static ArrayList getSinContestar()
         {
             ArrayList a = new ArrayList();
 
-            ENPeticionCRUD p1 = new ENPeticionCRUD(1, "categoria IB", "No existe la categoria IB", null, 2);
+            ENPeticionCRUD p1 = new ENPeticionCRUD(1, "categoria IB", "No existe la categoria IB", false, 2);
             a.Add(p1);
-            ENPeticionCRUD p2 = new ENPeticionCRUD(2, "error material", "No funciona el material ¿?", null, 1);
+            ENPeticionCRUD p2 = new ENPeticionCRUD(2, "error material", "No funciona el material ¿?", false, 1);
             a.Add(p2);
 
             return a;
-        }*/
+        }
+
+        public static ArrayList getContestadas()
+        {
+            ArrayList a = new ArrayList();
+
+            ENPeticionCRUD p1 = new ENPeticionCRUD(1, "categoria DPAA", "No existe la categoria DPAA", true, 2);
+            a.Add(p1);
+          
+
+            return a;
+        }
     }
 }
