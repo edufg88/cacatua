02c40f45028a1cffc176cc20c332209131d2c710
damianmp <damianmp@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-03-31 07:22:21 +0000

    PeticionCAD creado

---
+++ trunk/cacatUA/Libreria/PeticionCAD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Libreria
+{
+    class PeticionCAD
+    {
+        public static ArrayList getSinContestar()
+        {
+            ArrayList a = new ArrayList();
+
+            ENPeticionCRUD p1 = new ENPeticionCRUD(1, "categoria IB", "No existe la categoria IB", null, 2);
+            a.Add(p1);
+            ENPeticionCRUD p2 = new ENPeticionCRUD(2, "error material", "No funciona el material ¿?", null, 1);
+            a.Add(p2);
+
+            return a;
+        }
+    }
+}
