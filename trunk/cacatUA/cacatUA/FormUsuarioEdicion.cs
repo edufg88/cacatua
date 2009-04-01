c9f9e70064eb93fdbf2a36023ee0489547b12727
edufg88 <edufg88@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-01 22:35:09 +0000

    Hago un commit más que nada para dejar constancia de cambios. Se han eliminado las ventanas y ya hay usercontrols en los que me cago. Cristian, necesito un botoncito chulo de esos para ocultar/desplegar un menú.

---
+++ trunk/cacatUA/cacatUA/FormUsuarioEdicion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cacatUA
+{
+    public partial class FormUsuarioEdicion : UserControl
+    {
+        public FormUsuarioEdicion()
+        {
+            InitializeComponent();
+        }
+
+        private void button_buscarFirma_Click(object sender, EventArgs e)
+        {
+            // Código para buscar firmas
+            // Aquí cargamos el DataGridView
+        }
+
+        private void button_buscarGaleria_Click(object sender, EventArgs e)
+        {
+            // Código para buscar firmas
+            // Aquí cargamos el DataGridView
+        }
+
+        private void button_buscarMensaje_Click(object sender, EventArgs e)
+        {
+            // Código para buscar firmas
+            // Aquí cargamos el DataGridView
+        }
+
+        private void button_buscarPeticion_Click(object sender, EventArgs e)
+        {
+            // Código para buscar firmas
+            // Aquí cargamos el DataGridView
+        }
+
+        private void button_buscarEncuesta_Click(object sender, EventArgs e)
+        {
+            // Código para buscar firmas
+            // Aquí cargamos el DataGridView
+        }
+    }
+}
