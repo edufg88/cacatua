c6b4cd98ee5bae0918e478f398826614d1d38498
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-03 14:39:34 +0000

    Renombro ENRespuestaCRUD a ENRespuesta y convierto RespuestaCAD en singleton.

--- trunk/cacatUA/Libreria/RespuestaCAD.cs
+++ trunk/cacatUA/Libreria/RespuestaCAD.cs
@@ -2,10 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
 
 namespace Libreria
 {
+    /// <summary>
+    /// Clase singleton que realiza el acceso a la base de datos para manipular los hilos.
+    /// </summary>
     class RespuestaCAD
     {
+        private static RespuestaCAD instancia = null;
+        private string cadenaConexion;
+
+        /// <summary>
+        /// Obtiene la única instancia de la clase RespuestaCAD. Si es la primera vez
+        /// que se invoca el método, se crea el objeto; si no, sólo se devuelve la referencia
+        /// al objeto que ya fue creado anteriormente.
+        /// </summary>
+        /// <returns>Devuelve una referencia a la única instancia de la clase.</returns>
+        public static RespuestaCAD GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new RespuestaCAD();
+            }
+            return instancia;
+        }
+
+        /// <summary>
+        /// Constructor en el ámbito privado de la clase para no permitir más
+        /// de una instancia.
+        /// </summary>
+        private RespuestaCAD()
+        {
+            cadenaConexion = ConfigurationManager.ConnectionStrings["cacatua"].ConnectionString;
+        }
     }
 }
