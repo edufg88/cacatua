9bc506665a3509f191d20756d901f6b5f5a71ee2
damianmp <damianmp@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-06 15:24:46 +0000

    cambios de anotaciones en peticion

---
+++ cacatUA/Libreria/ENPeticion.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Libreria
+{
+    public class ENPeticion
+    {
+        private int id;
+        private string asunto;
+        private string texto;
+        private string respuesta;
+        private int usuario;
+        private DateTime fecha;
+        private bool contestada;
+
+        public ENPeticion()
+        {
+            id = 0;
+            asunto = "";
+            texto = "";
+            contestada = false;
+        }
+
+        public ENPeticion(int i,string a,string t,bool c,int u)
+        {
+            id = i;
+            asunto = a;
+            texto = t;
+            contestada = c;
+            usuario = u;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Asunto
+        {
+            get { return asunto; }
+            set { asunto = value; }
+        }
+        public string Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+        public string Respuesta
+        {
+            get { return respuesta; }
+            set { respuesta = value; }
+        }
+        public int Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+        public bool Contestada
+        {
+            get { return contestada; }
+            set { contestada = true; }
+        }
+
+        public static ArrayList GetSinContestar()
+        {
+            return PeticionCAD.GetSinContestar();
+        }
+
+        public static ArrayList GetContestadas()
+        {
+            return PeticionCAD.GetContestadas();
+        }
+
+        public static ENPeticion GetPeticion(int id)
+        {
+            return PeticionCAD.GetPeticion(id);
+        }
+
+        public static bool ActualizarPeticion(ENPeticion p)
+        {
+            return PeticionCAD.ActualizarPeticion(p);
+        }
+
+        public static void BorrarPeticion(int id)
+        {
+            PeticionCAD.BorrarPeticion(id);
+        }
+    }
+}
