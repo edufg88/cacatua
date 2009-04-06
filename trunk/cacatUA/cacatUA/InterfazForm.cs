85f8dc3c5951aae3a95694eee3714b87e052cdc0
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-06 18:49:55 +0000

    Añadida una nueva interfaz (InterfazForm) de la que heredarán todos los formularios principales (FormForo, FormuUsuarios, FormCategorias, FormMateriales, FormGrupos y FormPeticiones). Básicamente hay que implementar un método Recibir y otro Enviar, para lo de "volver".

---
+++ trunk/cacatUA/cacatUA/InterfazForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cacatUA
+{
+    abstract public partial class InterfazForm : UserControl
+    {
+        abstract public Object Enviar();
+        abstract public void Recibir(Object objeto);
+    }
+}
