e956e6424a537b628eab01486a43c7560dd65826
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-15 15:29:09 +0000

    Más código en ENRespuesta y algunos métodos en FormForoRespuestas.

--- cacatUA/Libreria/ENRespuesta.cs
+++ cacatUA/Libreria/ENRespuesta.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace Libreria
 {
-    class ENRespuesta
+    public class ENRespuesta : InterfazEN
     {
         private int id;
         private String texto;
@@ -20,6 +21,123 @@ namespace Libreria
             autor = null;
             hilo = null;
             fecha = new DateTime();
+        }
+
+        /// <summary>
+        /// Obtiene las respuestas de un hilo. Si el hilo no tiene respuestas, se obtiene una lista
+        /// de longitud 0. Si ocurre un error, devuelve una lista nula.
+        /// </summary>
+        /// <param name="hilo">Hilo del que se van a obtener sus respuestas.</param>
+        /// <returns>
+        /// Devuelve una lista de respuestas (ArrayList de ENRespuesta). Si ocurre un error, esta
+        /// lista es null; si no, es una lista con 0 o más elementos.
+        /// </returns>
+        public static ArrayList Obtener(ENHilo hilo)
+        {
+            return RespuestaCAD.Instancia.Obtener(hilo);
+        }
+
+        /// <summary>
+        /// Obtiene una respuesta según el identificador indicado.
+        /// </summary>
+        /// <param name="id">Identificador de la respuesta que se va a obtener.</param>
+        /// <returns>Devuelve la respuesta del identificador seleccionado. Si falla, devuelve null.</returns>
+        public static ENRespuesta Obtener(int id)
+        {
+            return RespuestaCAD.Instancia.Obtener(id);
+        }
+
+        /// <summary>
+        /// Guarda una nueva respuesta en la base de datos. Se supone que esta respuesta no existe en la base de datos.
+        /// </summary>
+        /// <returns>Devuelve verdadero si se ha insertado correctamente.</returns>
+        override public bool Guardar()
+        {
+            int id = 0;
+            if (RespuestaCAD.Instancia.Guardar(this, out id))
+            {
+                this.id = id;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Borra la respuesta de la base de datos.
+        /// </summary>
+        /// <returns>
+        /// Devuelve verdadero si se ha borrado con éxito o falso si no se ha borrado.
+        /// Es posible que no se borre si no existía.
+        /// </returns>
+        override public bool Borrar()
+        {
+            if (RespuestaCAD.Instancia.Borrar(id))
+            {
+                id = 0;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Borra la respuesta de la base de datos.
+        /// </summary>
+        /// <param name="id">Identificador de la respuesta que se va a borrar.</param>
+        /// <returns>
+        /// Devuelve verdadero si se ha borrado con éxito o falso si no se ha borrado.
+        /// Es posible que no se borre si no existía.
+        /// </returns>
+        public static bool Borrar(int id)
+        {
+            if (RespuestaCAD.Instancia.Borrar(id))
+            {
+                id = 0;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Actualiza una respuesta que ya está creada en la base de datos.
+        /// </summary>
+        /// <returns>Devuelve verdadero si la respuesta ya existía y ha sido actualizada correctamente. Falso en caso contrario.</returns>
+        override public bool Actualizar()
+        {
+            return RespuestaCAD.Instancia.Actualizar(this);
+        }
+
+        /// <summary>
+        /// Realiza una consulta a la base de datos para obtener la cantidad de 
+        /// respuestas que hay en total en todo el foro.
+        /// </summary>
+        /// <returns>Devuelve un valor entero con la cantidad de hilos del foro.</returns>
+        public static int Cantidad()
+        {
+            return RespuestaCAD.Instancia.Cantidad();
+        }
+
+        /// <summary>
+        /// Realiza una consulta a la base de datos para obtener la cantidad de respuestas totales
+        /// de un hilo.
+        /// </summary>
+        /// <returns>Devuelve un valor entero con la cantidad de respuestas del hilo.</returns>
+        public static int Cantidad(ENHilo hilo)
+        {
+            return RespuestaCAD.Instancia.Cantidad(hilo);
+        }
+
+        /// <summary>
+        /// Obtiene la última respuesta de la base de datos.
+        /// </summary>
+        /// <returns>Devuelve la última respuesta insertada en la base de datos. Si falla, devuelve null.</returns>
+        public static ENRespuesta Ultimo()
+        {
+            return RespuestaCAD.Instancia.Ultimo();
+        }
+
+        public ENRespuesta Ultimo(ENHilo hilo)
+        {
+            return RespuestaCAD.Instancia.Ultimo(hilo);
         }
 
         public int Id
--- cacatUA/cacatUA/FormForoRespuestas.cs
+++ cacatUA/cacatUA/FormForoRespuestas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
@@ -21,6 +22,48 @@ namespace cacatUA
             this.hilo = hilo;
             label_foro.Text = "Respuestas del hilo nº " + hilo.Id;
             usuario = null;
+
+            Resultados = ENRespuesta.Obtener(hilo);
+
+            CambiarCrearNuevo();
+        }
+
+        public ArrayList Resultados
+        {
+            set
+            {
+                dataGridView_resultados.Rows.Clear();
+                ArrayList lista = value;
+                if (lista != null)
+                {
+                    for (int i = 0; i < lista.Count; i++)
+                    {
+                        DataGridViewRow fila = new DataGridViewRow();
+                        fila.CreateCells(dataGridView_resultados);
+
+                        ENRespuesta auxiliar = (ENRespuesta)lista[i];
+                        fila.Cells[0].Value = auxiliar.Id.ToString();
+                        fila.Cells[1].Value = auxiliar.Texto.ToString();
+                        fila.Cells[2].Value = auxiliar.Autor.Usuario.ToString();
+                        fila.Cells[3].Value = auxiliar.Fecha.ToString();
+                        dataGridView_resultados.Rows.Add(fila);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ocurrió un error con la base de datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }             
+            }
+        }
+
+        public void CambiarCrearNuevo()
+        {
+
+        }
+
+        public void CambiarEdicion(int id)
+        {
+
         }
 
         private void button_seccionCrear_Click(object sender, EventArgs e)
