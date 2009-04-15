930538e23e3d89bf64f01ec1c8f0db9a5032fbfc
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-15 15:37:06 +0000

    Requetearreglado el proyecto.

---
+++ cacatUA/cacatUA/InterfazForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cacatUA
+{
+    public partial class InterfazForm : UserControl
+    {
+        virtual public Object Enviar()
+        {
+            return null;
+        }
+
+        virtual public void Recibir(Object objeto)
+        {
+
+        }
+    }
+}
