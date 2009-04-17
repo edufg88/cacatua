eb0bba3e4522b81019b4f7e0ed671a0299b56500
jcz1710 <jcz1710@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-17 09:39:19 +0000

    Documentado ENCategoria.

--- cacatUA/Libreria/ENCategoria.cs
+++ cacatUA/Libreria/ENCategoria.cs
@@ -13,13 +13,20 @@ namespace Libreria
         private int padre;
 
 
-        //Constructores
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
 
         public ENCategoria()
         {
             id = 0;
         }
 
+        /// <summary>
+        /// Constructor a partir de una id. 
+        /// El objeto se instancia si dicha id esta en la base de datos
+        /// </summary>
+        /// <param name="id">Identificador de la categoria</param>
         public ENCategoria(int id)
         {
             ENCategoria aux = Obtener(id);
@@ -36,6 +43,12 @@ namespace Libreria
             }
         }
 
+        /// <summary>
+        /// Constructor con datos, usualmente para despues guardarlo en la base de datos.
+        /// </summary>
+        /// <param name="nombre">Nombre de la categoria</param>
+        /// <param name="descripcion">Descripcion de la categoria</param>
+        /// <param name="padre">Entero que identifica al padre de esta categoria</param>
         public ENCategoria(String nombre, String descripcion, int padre)
         {
             this.nombre = nombre;
@@ -43,6 +56,10 @@ namespace Libreria
             this.descripcion = descripcion;
         }
 
+        /// <summary>
+        /// Metodo que copia los datos de una categoria al objeto que llama.
+        /// </summary>
+        /// <param name="copia">Objeto ENCategoria a copiar</param>
         public void Copiar(ENCategoria copia)
         {
             id = copia.Id;
@@ -52,20 +69,31 @@ namespace Libreria
         }
 
 
-        //Metodos heredados de InterfazEN
+        /// <summary>
+        /// Guarda el objeto en la base de datos.
+        /// </summary>
+        /// <returns>Devuelve 'true' si el objeto se ha guardado correctamente.</returns>
 
         override public bool Guardar()
         {
             return CategoriaCAD.Instancia.Crear(this);
         }
 
+        /// <summary>
+        /// Actualiza el objeto en la base de datos.
+        /// </summary>
+        /// <returns>Devuelve 'true' si el objeto se ha actualizado correctamente.</returns>
         override public bool Actualizar()
         {
             return CategoriaCAD.Instancia.Actualizar(this);
         }
 
+        /// <summary>
+        /// Metodo encargado de borrar el objeto de la base de datos.
+        /// Antes de borrar el objeto en concreto se encarga de borrar los descendientes.
+        /// </summary>
+        /// <returns>Devuelve 'true' si el objeto se ha borrado correctamente.</returns>
         override public bool Borrar() {
-            //Borramos primero todos los hijos
             foreach (ENCategoria c in ObtenerHijos()) 
             {
                 if(!c.Borrar()) {
@@ -75,7 +103,10 @@ namespace Libreria
             return CategoriaCAD.Instancia.Borrar(this);
         }
 
-        //Metodos asociados a las categorias
+        /// <summary>
+        /// Metodo que obtiene el nombre completo (ruta + nombre) de la categoria.
+        /// </summary>
+        /// <returns>Devuelve una cadena con el nombre completo</returns>
 
         public String NombreCompleto()
         {
@@ -96,6 +127,11 @@ namespace Libreria
             }
         }
 
+        /// <summary>
+        /// Metodo encargo de obtener la ruta (de donde viene) de la categoria.
+        /// Se apoya en otros metodos, ya que la ruta es igual al nombre completo del padre.
+        /// </summary>
+        /// <returns>Devuelve una cadena con la ruta del objeto</returns>
         public String Ruta()
         {
             if (id != 0)
@@ -104,26 +140,49 @@ namespace Libreria
                 return "";
         }
 
+        /// <summary>
+        /// Metodo que obtiene una lista compuesta por todos los hijos de esta categoria.
+        /// </summary>
+        /// <returns>Devuelve la lista de ENCategoria hijos.</returns>
         public ArrayList ObtenerHijos()
         {
             return CategoriaCAD.Instancia.HijosDe(this);
         }
 
+        /// <summary>
+        /// Metodo que obtiene una lista compuesta por todos los usuarios suscritos a la categoria.
+        /// </summary>
+        /// <returns>Devuelve la lista de ENUsuario suscritos.</returns>
         public ArrayList UsuariosSuscritos()
         {
             return CategoriaCAD.Instancia.UsuariosSuscritosA(this);
         }
 
+        /// <summary>
+        /// Metodo que devuelve cuantos materiales hay actualmente en la base de datos asociados a esta categoria.
+        /// </summary>
+        /// <returns>Entero que indica el número de materiales.</returns>
         public int NumMateriales()
         {
             return CategoriaCAD.Instancia.NumMaterialesEn(this);
         }
 
+
+        /// <summary>
+        /// Metodo que devuelve cuantos hilos hay actualmente en la base de datos asociados a esta categoria.
+        /// </summary>
+        /// <returns>Entero que indica el número de hilos.</returns>
         public int NumHilos()
         {
             return CategoriaCAD.Instancia.NumHilosEn(this);
         }
 
+        /// <summary>
+        /// Metodo encargado de comprobar si la categoria que llama es descendiente de otra.
+        /// Para comprobarlo nos basamos en la ruta actual y la del padre (recursivamente) y el nombre completo de la categoria parámetro.
+        /// </summary>
+        /// <param name="cat">Objeto ENCategoria a comprobar si la categoria que llama es desciende de él.</param>
+        /// <returns>Devuelve 'true' si es descendiente.</returns>
         public bool EsDescendienteDe(ENCategoria cat)
         {
             if(Ruta() == cat.NombreCompleto()) {
@@ -139,8 +198,11 @@ namespace Libreria
                 
         }
 
-        //Metodos relacionados con la tabla suscripcion
-
+        /// <summary>
+        /// Suscribe un usuario a esta categoria y a todos sus descendientes (recursivamente).
+        /// </summary>
+        /// <param name="usuario">Objeto ENUsuario a suscribir.</param>
+        /// <returns>Devuelve 'true' si el usuario ha sido suscrito correctamente.</returns>
         public bool SuscribirUsuario(ENUsuario usuario)
         {
             if (CategoriaCAD.Instancia.AñadirSuscripcion(this, usuario))
@@ -160,28 +222,45 @@ namespace Libreria
             }
         }
 
-        public void DessuscribirUsuario(ENUsuario usuario)
+        /// <summary>
+        /// Insuscribe un usuario de esta categoria y de todos sus descendientes (recursivamente).
+        /// </summary>
+        /// <param name="usuario">Objeto ENUsuario a insuscribir.</param>
+        /// <returns>Devuelve 'true' si el usuario ha sido insuscrito correctamente.</returns>
+        public void InsuscribirUsuario(ENUsuario usuario)
         {
             CategoriaCAD.Instancia.QuitarSuscripcion(this, usuario);
 
             foreach (ENCategoria c in ObtenerHijos())
             {
-                c.DessuscribirUsuario(usuario);
+                c.InsuscribirUsuario(usuario);
             }
         }        
 
-        //Metodos estaticos de categorias
+        /// <summary>
+        /// Método estático que obtiene una categoria de la base de datos a partir de su identificador.
+        /// </summary>
+        /// <param name="id">Entero que identifica la categoria a buscar.</param>
+        /// <returns>Devuelve un objeto ENCategoria instanciado.</returns>
 
         public static ENCategoria Obtener(int id)
         {
             return CategoriaCAD.Instancia.Obtener(id);
         }
 
+        /// <summary>
+        /// Método estático que obtiene las categorias superiores (sin padre).
+        /// </summary>
+        /// <returns>Devuelve una lista con los objeto ENCategoria que son categoria superior.</returns>
         public static ArrayList CategoriasSuperiores()
         {
             return CategoriaCAD.Instancia.ObtenerSuperiores();
         }
 
+        /// <summary>
+        /// Método estático que obtiene el número de categorias creadas.
+        /// </summary>
+        /// <returns>Devuelve un entero que indica el número de categorias.</returns>
         public static int NumCategorias()
         {
             return CategoriaCAD.Instancia.NumCategorias();
