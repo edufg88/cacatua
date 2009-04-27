789225cd3b81809f9c730baef16bb668ef4f4cf6
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-27 15:56:17 +0000

    Diseño preliminar para la página maestra.

--- trunk/cacatUA/WebCacatUA/PaginaMaestra.Master.designer.cs
+++ trunk/cacatUA/WebCacatUA/PaginaMaestra.Master.designer.cs
@@ -40,74 +40,11 @@ public partial class PaginaMaestra {
     protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     
     /// <summary>
-    /// Control Image1.
+    /// Control ContentPlaceHolder_contenido.
     /// </summary>
     /// <remarks>
     /// Campo generado automáticamente.
     /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     /// </remarks>
-    protected global::System.Web.UI.WebControls.Image Image1;
-    
-    /// <summary>
-    /// Control DropDownList1.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
-    
-    /// <summary>
-    /// Control Label1.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.Label Label1;
-    
-    /// <summary>
-    /// Control TextBox1.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.TextBox TextBox1;
-    
-    /// <summary>
-    /// Control Label2.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.Label Label2;
-    
-    /// <summary>
-    /// Control TextBox2.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.TextBox TextBox2;
-    
-    /// <summary>
-    /// Control Button4.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.Button Button4;
-    
-    /// <summary>
-    /// Control ContentPlaceHolder_Contenido.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder_Contenido;
+    protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder_contenido;
 }
