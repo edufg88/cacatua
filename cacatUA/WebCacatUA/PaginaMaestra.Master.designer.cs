0c268b96e15b03eb1e6db0b16ba028d89f7f20f8
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-27 17:36:30 +0000

    Diseño de la página maestra finalizado. El recuadro negro que hay en el centro es el espacio que hay para cada apartado.

--- cacatUA/WebCacatUA/PaginaMaestra.Master.designer.cs
+++ cacatUA/WebCacatUA/PaginaMaestra.Master.designer.cs
@@ -40,6 +40,60 @@ public partial class PaginaMaestra {
     protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     
     /// <summary>
+    /// Control Label_iniciarSesion.
+    /// </summary>
+    /// <remarks>
+    /// Campo generado automáticamente.
+    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Label Label_iniciarSesion;
+    
+    /// <summary>
+    /// Control TextBox2.
+    /// </summary>
+    /// <remarks>
+    /// Campo generado automáticamente.
+    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox2;
+    
+    /// <summary>
+    /// Control TextBox1.
+    /// </summary>
+    /// <remarks>
+    /// Campo generado automáticamente.
+    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox TextBox1;
+    
+    /// <summary>
+    /// Control Button1.
+    /// </summary>
+    /// <remarks>
+    /// Campo generado automáticamente.
+    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button Button1;
+    
+    /// <summary>
+    /// Control Label_idioma.
+    /// </summary>
+    /// <remarks>
+    /// Campo generado automáticamente.
+    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Label Label_idioma;
+    
+    /// <summary>
+    /// Control DropDownList1.
+    /// </summary>
+    /// <remarks>
+    /// Campo generado automáticamente.
+    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+    
+    /// <summary>
     /// Control ContentPlaceHolder_contenido.
     /// </summary>
     /// <remarks>
