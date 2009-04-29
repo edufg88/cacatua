6391282980a29d070fdf2b6d239304f993f00a50
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-04-29 16:25:47 +0000

    Alguna cosilla hecha del foro, poco.

--- trunk/cacatUA/WebCacatUA/foro.aspx.cs
+++ trunk/cacatUA/WebCacatUA/foro.aspx.cs
@@ -10,11 +10,61 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using Libreria;
 
 public partial class foro : WebCacatUA.InterfazWeb
 {
+    private int pagina;
+    private int cantidad;
+    private ENCategoria categoria;
+    private bool orden;
+    private string ordenar;
+    private string busqueda;
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        extraerParametros();
+
+        Label1.Text = "Página: " + pagina;
+    }
+
+    /// <summary>
+    /// Extramos los parámetros de la URL.
+    /// La página actual, la columna de ordenador, si es ascendente o descendente, el filtro de búsqueda ...
+    /// </summary>
+    private void extraerParametros()
+    {
+        pagina = 1;
+        try
+        {
+            pagina = int.Parse(Request["pagina"].ToString());
+        }
+        catch (Exception) { }
+
+        cantidad = 5; // debería ser común para todas secciones, debería estar en la sesión este valor y lo ideal es que se pudiera cambiar (pero pasando...)
+
+        orden = true;
+        try
+        {
+            if (Request["orden"].ToString() == "descendente")
+                orden = false;
+        }
+        catch (Exception) { }
+
+        ordenar = "";
+        try
+        {
+            ordenar = Request["ordenar"].ToString();
+        }
+        catch (Exception) { }
+
+        busqueda = "";
+        try
+        {
+            busqueda = Request["busqueda"].ToString();
+        }
+        catch (Exception) { }
+
 
     }
 }
