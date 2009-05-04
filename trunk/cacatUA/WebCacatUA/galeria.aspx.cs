befe9682ca5937b149bd31ce82f385591e8e3a74
damianmp <damianmp@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-04 20:12:58 +0000

    por que no encuentro el mdf?

--- trunk/cacatUA/WebCacatUA/galeria.aspx.cs
+++ trunk/cacatUA/WebCacatUA/galeria.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using System.Xml.Linq;
+using Libreria;
 
 namespace WebCacatUA
 {
@@ -18,22 +19,35 @@ namespace WebCacatUA
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            ArrayList fotos = new ArrayList();
+            fotos = ENImagen.Obtener();
+
             Table1.Attributes.Add("widht", "90%");
             TableRow r = new TableRow();
             TableCell c = new TableCell();
 
             c.Controls.Add(new LiteralControl("<img src=\"/imagenes/botonanterioresfotos.png\" alt=\"\" />"));
             r.Controls.Add(c);
-
-            for (int i = 1; i <= 10; i++)
+            int cont=1;
+            foreach(ENImagen i in fotos)
             {
+            
                 c = new TableCell();
-                c.Controls.Add(new LiteralControl("<img onclick=\"funcion(" + i + ")\" height=\"38px\" src=\"/imagenes/" + i + ".jpg\" alt=\"\" />"));
+                c.Controls.Add(new LiteralControl("<img onclick=\"funcion(" + i.Id + ")\" height=\"38px\" src=\""+ i.Archivo +"\".jpg\" alt=\"" + i.Titulo + "\" />"));
                 r.Controls.Add(c);
+
+                if(cont%10==0)
+                {
+                    Table1.Controls.Add(r);
+                r = new TableRow();
+                }
+
+                cont++;
+                
             }
             
-            c = new TableCell();
-            c.Controls.Add(new LiteralControl("<td><img src=\"/imagenes/botonsiguientesfotos.png\" alt=\"\" /></td>"));
+            //c = new TableCell();
+            //c.Controls.Add(new LiteralControl("<td><img src=\"/imagenes/botonsiguientesfotos.png\" alt=\"\" /></td>"));
             r.Controls.Add(c);
 
             Table1.Controls.Add(r);
