d3849de60f5e40b6748679d44e26cec77bf0e845
edufg88 <edufg88@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-04 22:16:08 +0000

    Subo la página de registro de usuario. Ahora valida como DIOS! estoy aún haciendo pruebecillas... Esa peña

---
+++ cacatUA/WebCacatUA/registroUsuario.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/PaginaMaestra.Master" AutoEventWireup="true" CodeBehind="registroUsuario.aspx.cs" Inherits="registroUsuario" Title="Registro de usuario - CacatUA" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="estilos/registroUsuario.css" media="screen" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder_titulo" runat="server">
+    <div id="cabeceraRegistro">
+        <p>
+        Registro de usuario
+        </p>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder_contenido" runat="server">
+    <div id="contenido">
+        <p class="etiqueta">Nick de usuario</p>
+        <p>    
+            <asp:TextBox ID="TextBox_usuario" runat="server"></asp:TextBox>
+            <asp:Label ID="Label_infoUsuario" runat="server" ForeColor="Red" CssClass="infoError"></asp:Label>
+        </p>
+        <p class="etiqueta">Contraseña</p>
+        <p>
+            <asp:TextBox ID="TextBox_contrasena" TextMode="Password" runat="server"></asp:TextBox>
+            <asp:Label ID="Label_infoContrasena" runat="server" ForeColor="Red" CssClass="infoError"></asp:Label>
+        </p>
+        <p class="etiqueta">Confirmar contraseña</p>
+        <p>
+            <asp:TextBox ID="TextBox_confirmarContrasena" TextMode="Password" runat="server"></asp:TextBox>
+            <asp:Label ID="Label_infoConfContrasena" runat="server" ForeColor="Red" CssClass="infoError"></asp:Label>
+        </p>
+        <p class="etiqueta">Nombre</p>
+        <p>
+            <asp:TextBox ID="TextBox_nombre" runat="server"></asp:TextBox>
+            <asp:Label ID="Label_infoNombre" runat="server" ForeColor="Red" CssClass="infoError"></asp:Label>
+        </p>
+        <p class="etiqueta">DNI</p>
+        <p>
+            <asp:TextBox ID="TextBox_dni" runat="server"></asp:TextBox>
+            <asp:Label ID="Label_infoDni" runat="server" ForeColor="Red" CssClass="infoError"></asp:Label>
+        </p>
+        <p class="etiqueta">Correo</p>
+        <p>
+            <asp:TextBox ID="TextBox_correo" runat="server"></asp:TextBox>
+            <asp:Label ID="Label_infoCorreo" runat="server" ForeColor="Red" CssClass="infoError"></asp:Label>
+        </p>
+        <p class="etiqueta">Información Adicional</p>
+        <p>
+            <asp:TextBox ID="TextBox_adicional" TextMode="MultiLine" Columns="20" Rows="4" runat="server"></asp:TextBox>
+            <asp:Label ID="Label_infoAdicional" runat="server" ForeColor="Red" CssClass="infoError"></asp:Label>
+        </p>
+    
+        <p class="pie">       
+            <asp:Button ID="Button_confirmar" runat="server" Text="Confirmar" 
+                onclick="Button_confirmar_Click" />
+            <asp:Button ID="Button_limpiar" runat="server" Text="Limpiar" 
+                onclick="Button_limpiar_Click" />   
+        </p>
+    </div>
+</asp:Content>
---
+++ cacatUA/WebCacatUA/registroUsuario.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+using Libreria;
+
+public partial class registroUsuario : WebCacatUA.InterfazWeb
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void LimpiarErrores()
+    {
+        Label_infoUsuario.Text = "";
+        Label_infoContrasena.Text = "";
+        Label_infoConfContrasena.Text = "";
+        Label_infoNombre.Text = "";
+        Label_infoCorreo.Text = "";
+        Label_infoDni.Text = "";
+        Label_infoAdicional.Text = "";
+    }
+    protected bool ValidarCampos()
+    {
+        bool valido = true;
+        string error = "";
+
+        LimpiarErrores();
+
+        error = ENUsuario.ValidarFormulario("usuario", TextBox_usuario.Text);
+        Label_infoUsuario.Text = ENUsuario.ValidarFormulario("usuario", TextBox_usuario.Text);
+        error = ENUsuario.ValidarFormulario("contrasena", TextBox_contrasena.Text);
+        Label_infoContrasena.Text = ENUsuario.ValidarFormulario("contrasena", TextBox_contrasena.Text);
+        error = ENUsuario.ValidarFormulario("nombre", TextBox_nombre.Text); 
+        Label_infoNombre.Text = ENUsuario.ValidarFormulario("nombre", TextBox_nombre.Text);
+        error = ENUsuario.ValidarFormulario("correo", TextBox_correo.Text);
+        Label_infoCorreo.Text = ENUsuario.ValidarFormulario("correo", TextBox_correo.Text);
+        error = ENUsuario.ValidarFormulario("dni", TextBox_dni.Text);
+        Label_infoDni.Text = ENUsuario.ValidarFormulario("dni", TextBox_dni.Text);
+        error = ENUsuario.ValidarFormulario("adicional", TextBox_adicional.Text);
+        Label_infoAdicional.Text = ENUsuario.ValidarFormulario("adicional", TextBox_adicional.Text);
+
+        // Comprobamos que la confirmación de la contraseña está bien
+        if (TextBox_contrasena.Text != TextBox_confirmarContrasena.Text)
+        {
+            Label_infoConfContrasena.Text = "La contraseña y su confirmación no coinciden";
+            valido = false;
+        }
+        else
+        {
+            if (error != "")
+            {
+                valido = false;
+            }
+        }
+
+        return valido;
+    }
+
+    protected void Button_confirmar_Click(object sender, EventArgs e)
+    {
+        if (ValidarCampos())
+        {
+            ENUsuario us = ENUsuario.Obtener(TextBox_usuario.Text);
+
+            if (us == null)
+            {
+                us = new ENUsuario();
+
+                us.Usuario = TextBox_usuario.Text;
+                us.Contrasena = TextBox_contrasena.Text;
+                us.Nombre = TextBox_nombre.Text;
+                us.Dni = TextBox_dni.Text;
+                us.Correo = TextBox_correo.Text;
+                us.Adicional = TextBox_adicional.Text;
+
+                us.Guardar();
+            }
+            else // El usuario ya existe
+            {
+                Label_infoUsuario.Text = "El nombre de usuario ya existe";
+            }
+        }
+    }
+
+    protected void Button_limpiar_Click(object sender, EventArgs e)
+    {
+        TextBox_usuario.Text = "";
+        TextBox_contrasena.Text = "";
+        TextBox_confirmarContrasena.Text = "";
+        TextBox_nombre.Text = "";
+        TextBox_correo.Text = "";
+        TextBox_dni.Text = "";
+        TextBox_adicional.Text = "";
+    }
+}
+
