2264464d9f12a1b1eec301a2e3898683ef8b4aca
damianmp <damianmp@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-05 08:43:37 +0000

    añadidos ENImagenComentario y ImagenComentarioCAD

---
+++ cacatUA/Libreria/ENImagenComentario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Libreria
+{
+    class ENImagenComentario
+    {
+        private int id;
+        private string texto;
+        private DateTime fecha;
+        private ENUsuario usuario;
+        private ENImagen imagen;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+        public ENUsuario Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        public ENImagen Imagen
+        {
+            get { return imagen; }
+            set { imagen = value; }
+        }
+
+        public ENImagenComentario()
+        {
+
+        }
+
+        public ENImagenComentario(int i,string t,DateTime f,ENUsuario u,ENImagen img)
+        {
+            id = i;
+            texto = t;
+            fecha = f;
+            usuario = u;
+            imagen = img;
+        }
+
+        public static ArrayList Obtener(int imagen)
+        {
+            return ImagenComentarioCAD.Instancia.Obtener(imagen);
+        }
+    }
+}
---
+++ cacatUA/Libreria/ImagenComentarioCAD.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Configuration;
+
+namespace Libreria
+{
+    sealed class ImagenComentarioCAD
+    {
+        private static readonly ImagenComentarioCAD instancia = new ImagenComentarioCAD();
+        private String cadenaConexion;
+
+        // Devuelve la instancia única de la clase
+        public static ImagenComentarioCAD Instancia
+        {
+            get { return(instancia); }
+        }
+
+        // El constructor privado crea la cadena de conexión
+        private ImagenComentarioCAD()
+        {
+            cadenaConexion = ConfigurationManager.ConnectionStrings["cacatua"].ConnectionString;
+        }
+
+        public ENImagenComentario ObtenerDatos(SqlDataReader dr)
+        {
+            ENImagenComentario comentario = new ENImagenComentario();
+
+            comentario.Id = int.Parse(dr["id"].ToString());
+            comentario.Texto = dr["texto"].ToString();
+            comentario.Imagen = ENImagen.Obtener(dr["imagen"].ToString());
+            comentario.Usuario = ENUsuario.Obtener(int.Parse(dr["usuario"].ToString()));
+            comentario.Fecha = DateTime.Parse(dr["fecha"].ToString());
+
+            return (comentario);
+        }
+
+        public ArrayList Obtener(int imagen)
+        {
+            ArrayList comentarios = new ArrayList();
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand comando = new SqlCommand();
+                comando.Connection = conexion;
+                comando.CommandText = "SELECT * FROM imagenescomentarios where imagen=" + imagen;
+                SqlDataReader dr = comando.ExecuteReader();
+                // Generamos el ArrayList a partir del DataReader
+                while (dr.Read())
+                {
+                    ENImagenComentario comentario = ObtenerDatos(dr);
+                    comentarios.Add(comentario);
+                }
+
+                conexion.Close();
+            }
+            catch (SqlException)
+            {
+                Console.Write("Excepción obtener comentarios imagenes");
+            }
+
+            return comentarios;
+        }
+    }
+}
