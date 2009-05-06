01025e0395e523a6090fad22ee0874f8bed753f9
jcz1710 <jcz1710@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-06 15:26:17 +0000

    Rollos raros, ahora va todo bien. Creada tambien la pagina de Categorias de un usuario (ahora mismo vacio).

---
+++ trunk/cacatUA/WebCacatUA/categoriasUsuario.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/PaginaMaestraUsuario.Master" AutoEventWireup="true" CodeBehind="categoriasUsuario.aspx.cs" Inherits="WebCacatUA.categoriasUsuario" Title="Categorias de usuario - CacatUA"%>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder_headUsuario" runat="server">
+    <link rel="stylesheet" type="text/css" href="estilos/categoriasUsuario.css" media="screen" />
+</asp:Content>
---
+++ trunk/cacatUA/WebCacatUA/encuestasEdicion.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using Libreria;
+
+namespace WebCacatUA
+{
+    public partial class encuestasEdicion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String u = (String)Session["usuario"];
+            if (u != null)
+            {
+                ENUsuario usuario = ENUsuario.Obtener(u);
+                Label_Texto.Text = "Bienvenido " + usuario.Usuario;
+            }
+            else
+            {
+                Response.Redirect("index.aspx");
+            }
+        }
+    }
+}
