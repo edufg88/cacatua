df55c8280d583b89b8646b2ba722d2e428ae6302
jose.va18 <jose.va18@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-09 10:31:06 +0000

    añadido referencia al material

--- trunk/cacatUA/WebCacatUA/mostrarMaterial.aspx.designer.cs
+++ trunk/cacatUA/WebCacatUA/mostrarMaterial.aspx.designer.cs
@@ -104,6 +104,15 @@ namespace WebCacatUA {
         protected global::System.Web.UI.WebControls.Label Label_descripcion;
         
         /// <summary>
+        /// Label_referencia control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_referencia;
+        
+        /// <summary>
         /// ImageButton1 control.
         /// </summary>
         /// <remarks>
