8a5b4f50243ac2f6a40e8cbdc8915d5f4bd6ca98
jcz1710 <jcz1710@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-09 07:46:57 +0000

    Las opciones de una encuesta ya se pueden añadir y borrar (editarencuesta.aspx?id=1 por si quereis verlo).

--- trunk/cacatUA/WebCacatUA/editarencuesta.aspx.designer.cs
+++ trunk/cacatUA/WebCacatUA/editarencuesta.aspx.designer.cs
@@ -39,5 +39,23 @@ namespace WebCacatUA {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Table Table_encuesta;
+        
+        /// <summary>
+        /// Control TextBox_crearopcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_crearopcion;
+        
+        /// <summary>
+        /// Control Button_crearopcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button_crearopcion;
     }
 }
