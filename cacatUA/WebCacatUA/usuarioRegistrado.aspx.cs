66bc19325571b5d815cfa991b7eadeebeaf62b60
edufg88 <edufg88@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-10 11:51:48 +0000

    Es que... no iba bien una cosita

--- cacatUA/WebCacatUA/usuarioRegistrado.aspx.cs
+++ cacatUA/WebCacatUA/usuarioRegistrado.aspx.cs
@@ -11,13 +11,16 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using System.Xml.Linq;
 
-namespace WebCacatUA
-{
-    public partial class Formulario_web12 : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
 
-        }
+public partial class usuarioRegistrado : WebCacatUA.InterfazWeb
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LinkButton_volverInicio_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("index.aspx");
     }
 }
--- cacatUA/WebCacatUA/usuarioRegistrado.aspx.designer.cs
+++ cacatUA/WebCacatUA/usuarioRegistrado.aspx.designer.cs
@@ -11,4 +11,13 @@
 
 
 public partial class usuarioRegistrado {
+    
+    /// <summary>
+    /// Control LinkButton_volverInicio.
+    /// </summary>
+    /// <remarks>
+    /// Campo generado automáticamente.
+    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.LinkButton LinkButton_volverInicio;
 }
