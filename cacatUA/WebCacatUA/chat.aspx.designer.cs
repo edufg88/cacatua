f83cebfde7a98f82a4df659d223c613c8475a3bb
cristian64 <cristian64@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-15 12:35:11 +0000

    Arreglado el chat. Ya se introducen los mensajes bien, y se pone el scroll abajo. También se puede poner negritas y no hay que pinchar en Enviar para escribir, funciona al pelo.

--- cacatUA/WebCacatUA/chat.aspx.designer.cs
+++ cacatUA/WebCacatUA/chat.aspx.designer.cs
@@ -68,6 +68,33 @@ namespace WebCacatUA {
         protected global::System.Web.UI.WebControls.TextBox TextBox_mensajes;
         
         /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// Control Panel_mensajes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel_mensajes;
+        
+        /// <summary>
+        /// Control Panel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
         /// Control TextBox_mensaje.
         /// </summary>
         /// <remarks>
