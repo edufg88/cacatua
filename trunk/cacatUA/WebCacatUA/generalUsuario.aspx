1f73154ac507875a458ca711de8a287df2cf4825
edufg88 <edufg88@e939aff6-01af-11de-b507-8b4ecd5210be>
2009-05-17 18:20:19 +0000

    Hago commit porque he hecho mil cambios y si no lo hago flipais con conflictos... Si veis cosas raras no os preocupeis

---
+++ trunk/cacatUA/WebCacatUA/generalUsuario.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/PaginaMaestraUsuario.Master" AutoEventWireup="true" CodeFile="generalUsuario.aspx.cs" Inherits="generalUsuario" Title="CacatUA" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder_headUsuario" Runat="Server">
+    <link rel="stylesheet" type="text/css" href="estilos/generalUsuario.css" media="screen" />
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder_tituloUsuario" Runat="Server">
+<p><asp:Label ID="Label_nombreUsuario" runat="server" Text="<%$ Resources: I18N, TablonUsuario %>"></asp:Label></p>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder_contenidoUsuario" Runat="Server">
+    <div id="galeria">
+        <div id="infoGaleria">
+            <asp:LinkButton ID="LinkButton_infoGaleria" CssClass="textoInfo" runat="server" 
+                Text="<%$ Resources: I18N, UltimasImagenes %>" onclick="LinkButton_infoGaleria_Click"></asp:LinkButton>
+        </div>
+        <div id="contenidoGaleria">
+            <asp:Table ID="Table_galeria" runat="server" CssClass="tablaGaleria" 
+                CellPadding="0" CellSpacing="0"></asp:Table>
+        </div>
+    </div>
+    <div id="firmas">
+        <div id="infoFirmas">
+            <asp:LinkButton ID="LinkButton_infoFirmas" CssClass="textoInfo" runat="server" 
+                Text="<%$ Resources: I18N, UltimasFirmas %>" onclick="LinkButton_infoFirmas_Click"></asp:LinkButton>
+        </div>
+        <div id="contenidoFirmas">
+            <asp:Table ID="Table_firmas" runat="server" CssClass="tablaFirmas" 
+                CellPadding="0" CellSpacing="0"></asp:Table>
+        </div>
+    </div>
+</asp:Content>
---
+++ trunk/cacatUA/WebCacatUA/generalUsuario.aspx.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+using Libreria;
+
+
+public partial class generalUsuario : WebCacatUA.InterfazWeb
+{
+    private ENUsuario u;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        extraerParametros();
+
+        if (u != null)
+        {
+            // Comprobamos si es una petición nueva o el resultado de un evento del usuario.
+            if (!Page.IsPostBack)
+            {
+                cargarGaleria(); // Cargamos las últimas imágenes y las últimas firmas
+                cargarFirmas();
+            }
+        }
+        else
+        {
+            Response.Redirect("usuarios.aspx");
+        }
+    }
+
+    private void cargarGaleria()
+    {
+        ArrayList Imagenes = ENImagen.Obtener(u.Id, 1, 8);
+        Table_galeria.Controls.Clear();
+        int cont = 0;
+
+        TableRow fila = new TableRow();
+        TableRow fila2 = new TableRow();
+        foreach (ENImagen imagen in Imagenes)
+        {
+            // Columna de la imágen activa del usuario
+            TableCell c1 = new TableCell();
+            c1.CssClass = "columnaGaleria";
+            Label l1 = new Label();
+            if (imagen.Id == -1) // Comprobamos si tiene imagen activa el usuario
+            {
+                l1.Text = "<img src=\"imagenes/sinImagen.png\" alt=\"Foto de usuario\" width=\"60\" height=\"60\" class=\"imagen\" />";
+            }
+            else
+            {
+                l1.Text = "<img src=\"imagenes/" + imagen.Id + ".jpg\" width=\"100\" height=\"60\" alt=\"Foto de usuario\" class=\"imagen\" />";
+            }
+            Panel p1 = new Panel();
+            p1.Controls.Add(l1);
+            c1.Controls.Add(p1);
+
+            if (cont < 4)
+            {
+                fila.Controls.Add(c1);
+                Table_galeria.Controls.Add(fila);
+            }
+            else
+            {
+                fila2.Controls.Add(c1);
+                Table_galeria.Controls.Add(fila2);
+            }
+
+            cont++;
+        }
+    }
+
+    private void cargarFirmas()
+    {
+        ArrayList Firmas = ENFirma.ObtenerFirmas(u.Usuario, 1, 10);
+        Table_firmas.Controls.Clear();
+
+        foreach (ENFirma firma in Firmas)
+        {
+            // Fila con la info de la firma y su fecha
+            TableCell celda1 = new TableCell();
+            celda1.CssClass = "infoFirma";
+            Label emisor = new Label();
+            Label fecha = new Label();
+            emisor.Text = firma.Emisor.Usuario.ToString();
+            fecha.Text = firma.Fecha.ToString();
+            emisor.CssClass = "emisorFirma";
+            fecha.CssClass = "fechaFirma";
+            celda1.Controls.Add(emisor);
+            celda1.Controls.Add(fecha);
+            TableRow fila = new TableRow();
+            fila.Cells.Add(celda1);
+            Table_firmas.Rows.Add(fila);
+
+            // Fila con el texto de la firma
+            TableCell celda2 = new TableCell();
+            celda2.CssClass = "contenidoFirma";
+            Label texto = new Label();
+            texto.Text = firma.Texto.ToString();
+            texto.CssClass = "textoFirma";
+            celda2.Controls.Add(texto);
+            TableRow fila2 = new TableRow();
+            fila2.Cells.Add(celda2);
+            Table_firmas.Rows.Add(fila2);
+        }
+    }
+
+    /// <summary>
+    /// Extraemos el usuario y el modo ("normal" o "editar").
+    /// </summary>
+    private void extraerParametros()
+    {
+        // Intentamos extraer el usuario según el nombre recibido en los parámetros.
+        u = null;
+        try
+        {
+            u = ENUsuario.Obtener(Request["usuario"].ToString());
+        }
+        catch (Exception) { }
+
+        // Si no lo hemos extraido de ninguna forma, lo intentamos desde la sessión.
+        if (u == null)
+        {
+            try
+            {
+                u = ENUsuario.Obtener(Session["usuario"].ToString());
+            }
+            catch (Exception) { }
+        }
+    }
+
+
+    protected void LinkButton_infoFirmas_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("firmas.aspx?usuario=" + Session["usuario"]);
+    }
+    protected void LinkButton_infoGaleria_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("galeria.aspx?usuario=" + Session["usuario"]);
+    }
+}
